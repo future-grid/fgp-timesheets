d526b71187a8bb27411998b90e5acf6d58fadc93
Aaron <aaronpierce@outlook.com.au>
2020-01-10 14:34:46 +1100

    built reporter to spec

--- report/report.py
+++ report/report.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 import openpyxl as op
-import datetime
-import os
+import time, datetime, os, csv, json, sys
 
 # function will return an object with the date of the friday, and the initials of the creator
 # "filepath" is expected to be of form "TimeSheet_wYYYYMMDD_FL" FL = initials
 def get_data_from_title(filepath):
-    title = filepath[len("timesheets\\TimeSheet_w"):-len(".xlsx")]
+    title = filepath[len("timesheets/TimeSheet_w"):-len(".xlsx")]
     initials = title[-2:]
     date = datetime.datetime.strptime(title[:-3], '%Y%m%d')
+    date = date.timetuple()
+    date = time.strftime('%d-%m-%y', date)
 
     return {"initials": initials, "date": date}
 
@@ -17,7 +18,10 @@ def get_data_from_title(filepath):
 def get_day_from_data(row,friday_date):
     # this array defines the difference between the given day and friday. eg friday-friday = 0, friday-wednesday=2
     week_order = ["Friday", "Thursday", "Wednesday", "Tuesday", "Monday", "Sunday"]
+    friday_date = datetime.datetime.strptime(friday_date, '%d-%m-%y')
     date = friday_date - datetime.timedelta(days=week_order.index(row[0])) # row[0] is always the day of the week eg. "Tuesday"
+    date = date.timetuple()
+    date = time.strftime('%d-%m-%y', date)
 
     projects=[]
     for i in range(2, len(row)-1, 3):
@@ -33,11 +37,9 @@ def iter_rows(ws):
         yield [cell.value for cell in row]
 
 def read_timesheet(filepath):
-    print("Loading " + filepath)
     wb = op.load_workbook(filepath)
-    ws = wb.active # get the active worksheet
+    ws = wb.active # get the active worksheet (there will be only one worksheet per timesheet)
     if ws.cell(row=4,column=1).value == "=A3+1":
-        print("Old timesheet version!")
         ws.delete_cols(1)
 
     titledata = get_data_from_title(filepath)
@@ -49,100 +51,121 @@ def read_timesheet(filepath):
 
     return {"initials": titledata["initials"], "friday": titledata["date"], "days":days}
 
+def save_sheets(sheet_list):
+    try:
+        with open('cache.json', 'w') as fpath:
+            json.dump({'sheets' : sheet_list}, fpath)
+    except OSError as err:
+        print("Got an exception!")
+        print(err)
+
 def load_sheets(root_path):
-    directory = os.fsencode(root_path)
-    sheets = []
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith(".xlsx"):
-            try:
-                sheets.append(read_timesheet(root_path + "\\" + filename))
-            except Exception as e:
-                print("Could not read timesheet " + filename + "!")
-                print(e)
-            continue
-        else:
-            continue
+    if '--load-cache' in sys.argv:
+        print("Loading from cache.json")
+        with open('cache.json', "r") as fpath:
+            contents = json.load(fpath)
+            sheets = contents["sheets"]
+    else:
+        directory = os.fsencode(root_path)
+        sheets = []
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".xlsx"):
+                try:
+                    sheets.append(read_timesheet(root_path + "/" + filename))
+                except Exception as e:
+                    print("Could not read timesheet " + filename + "!")
+                    print(e)
+
+    if '--cache' in sys.argv:
+        save_sheets(sheets)
 
     return sheets
 
-def generate_project_overview(sheets):
-    projects = {}
+# this function will return the earliest date from a list of sheets
+def get_date(sheets, looking_for="earliest"):
+    best_date = None
     for sheet in sheets:
         for day in sheet["days"]:
-            for project in day["projects"]:
-                project_title = project["project"]
-                project_hours = float(project["hours"])
-                if project_title in projects:
-                    projects[project_title] += project_hours
-                else:
-                    projects[project_title] = project_hours
-
-    return projects
-
-def generate_employee_overview(sheets):
-    employees = {}
-    for sheet in sheets:
-        employee = sheet["initials"]
-        weekly_hours = 0
-        date = str(sheet["friday"])[:-9]
-
-        if employee not in employees:
-            employees[employee] = {}
-        
-        for day in sheet["days"]:
-            for project in day["projects"]:
-                weekly_hours += project["hours"]
-        
-        employees[employee][date] = weekly_hours
-
-    return employees
-
-def get_earliest_date(sheets):
-    early_date = None
-    for sheet in sheets:
-        for day in sheet["days"]:
-            date = day["date"]
-            if early_date is None:
-                early_date = date
-            if date < early_date:
-                early_date = date
-    return early_date
-
-def get_latest_date(sheets):
-    late_date = None
-    for sheet in sheets:
-        for day in sheet["days"]:
-            date = day["date"]
-            if late_date is None:
-                late_date = date
-            if date > late_date:
-                late_date = date
-    return late_date
+            date = datetime.datetime.strptime(day['date'], '%d-%m-%y')
+            best_date = date if best_date is None else best_date
+            if looking_for is "earliest":
+                best_date = date if date < best_date else best_date
+            elif looking_for is "latest":
+                best_date = date if date > best_date else best_date
+    return best_date
 
 def generate_timeline_overview(sheets):
-    early_date = get_earliest_date(sheets)
-    late_date = get_latest_date(sheets) + datetime.timedelta(days=1)
+    early_date = get_date(sheets, looking_for="earliest")
+    late_date = get_date(sheets, looking_for="latest") + datetime.timedelta(days=1)
     current_date = early_date
-    timeline = {}
+
+    first_row = ["Date", "Employee", "Project", "Hours", "Description"]
+    rows = [first_row]
+
+    # iterate over days
     while True:
-        date = str(current_date)[:-9]
+        date = datetime.datetime.strftime(current_date, '%d-%m-%y')
+        # increase day count by one, if we are at end - stop
+        current_date = current_date + datetime.timedelta(days=1)
         for sheet in sheets:
             employee = sheet["initials"]
             for day in sheet["days"]:
-                if day["date"] == current_date:
-                    if date not in timeline:
-                        timeline[date] = []
-                    for project in day["projects"]: # get all projects, hours and descriptions
-                        timeline[date].append(employee + ", " + project["project"] + " (" + str(project["hours"]) + "): " + str(project["description"]))
+                if day["date"] == date:
+                    projects = []
+                    for project in day["projects"]: # iterate over projects on day
+                        project_code = project["project"]
+                        project_hours = float(project["hours"])
+                        project_description = project["description"]
+                        projects_concatenated = False
 
-        
-        # iterate over the days, get all descriptions
-        current_date = current_date + datetime.timedelta(days=1)
-        if current_date == late_date:
+                        for p in projects:
+                            if p["project_code"] == project_code:
+                               # concat descriptions and hours
+                               p["hours"].append(project_hours)
+                               p["description"].append(project_description)
+                               projects_concatenated = True
+
+                        # if we've already added the project to another one, don't double count
+                        if projects_concatenated:
+                            continue
+
+                        projects.append({
+                            'date' : date,
+                            'employee' : employee,
+                            'project_code' : project_code,
+                            'hours' : [project_hours],
+                            'description' : [project_description]
+                        })
+
+                    for project in projects:
+                        description = ''
+                        hours = 0
+                        date = project["date"]
+                        employee = project["employee"]
+                        project_code = project["project_code"]
+                        if len(project["description"]) > 1:
+                            hour_list = project["hours"]
+                            description_list = project["description"]
+
+                            for hour, desc in zip(hour_list, description_list):
+                                description += (desc + " ("+str(hour)+"), ")
+                                hours += hour
+                            description = description[:-2] # remove trailing comma
+                        else:
+                            description = project["description"][0]
+                            hours = project["hours"][0]
+                        rows.append ([
+                            date, employee, project_code, hours, description
+                        ])
+
+        if current_date > late_date:
             break
 
-    return timeline
+        # save file as csv
+        with open("report.csv", "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(rows)
 
 def adjust_column_widths(ws):
     for column_cells in ws.columns:
@@ -162,44 +185,5 @@ def write_timeline_overview(ws_t, timeline_overview):
         
         i += 1
 
-def generate_report(root_path):
-    sheets = load_sheets(root_path)
-    employee_overview = generate_employee_overview(sheets)
-    project_overview = generate_project_overview(sheets)
-    timeline_overview = generate_timeline_overview(sheets)
-
-    wb = op.Workbook()
-    ws_p = wb.create_sheet("Project Overview")
-    ws_e = wb.create_sheet("Employee Overview")
-    ws_t = wb.create_sheet("Timeline Overview")
-    del wb["Sheet"]
-
-    i = 1
-    for employee, weeks in employee_overview.items():
-        emp_cell = ws_e.cell(row=i, column=1)
-        emp_cell.value = employee
-
-        a = 2
-        for week, hours in weeks.items():
-            week_cell = ws_e.cell(row=i, column=a)
-            week_cell.value = week + " - " + str(hours)
-            a += 1
-        i += 1
-
-    i = 1
-    for project, hours in project_overview.items():
-        pro_cell = ws_p.cell(row=i, column=1)
-        pro_cell.value = project
-
-        hour_cell = ws_p.cell(row=i, column=2)
-        hour_cell.value = hours
-        i += 1
-
-    write_timeline_overview(ws_t, timeline_overview)
-    adjust_column_widths(ws_p)
-    adjust_column_widths(ws_e)
-    adjust_column_widths(ws_t)
-    
-    wb.save('report.xlsx')
-
-generate_report("timesheets")
+sheets = load_sheets("timesheets")
+generate_timeline_overview(sheets)
